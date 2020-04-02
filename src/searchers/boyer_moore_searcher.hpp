03aed1f4983835155c74683050f6328e948a238a
Denis Pronin <dannftk@yandex.ru>
2020-04-02 13:01:24 +0300

    Added more descriptions to the searcher classes

--- src/searchers/boyer_moore_searcher.hpp
+++ src/searchers/boyer_moore_searcher.hpp
@@ -175,6 +175,13 @@ private:
 };
 
 
+///
+/// @brief      Searcher class that uses a pattern given to seek a subrange in an input range
+///
+/// @details    Searching algorithm is based on Boyer-Moore algorithm borrowed from boost library
+///
+/// @tparam     Pattern
+///
 template<typename Pattern>
 class BoyerMooreSearcher<Pattern, searchers::Boosted>
 {
@@ -182,13 +189,32 @@ public:
     explicit BoyerMooreSearcher(Pattern pattern) : pattern_(pattern) , searcher_(std::begin(pattern_), std::end(pattern_))
     {}
 
-    template<typename BidirIterator>
-    auto operator()(BidirIterator first, BidirIterator last) const noexcept
+    ///
+    /// @brief      Finds the first occurrence of the pattern in the input range
+    ///
+    /// @param[in]  first   The first random access iterator of the range to explore
+    /// @param[in]  last    The last random access iterator of the range to explore
+    ///
+    /// @tparam     RandomAccessIterator   Random Access Iterator
+    ///
+    /// @return     A subrange of input's iterators that meets the pattern
+    ///
+    template<typename RandomAccessIterator>
+    auto operator()(RandomAccessIterator first, RandomAccessIterator last) const noexcept
     {
         auto match = searcher_(first, last);
         return boost::make_iterator_range(match.first, match.second);
     }
 
+    ///
+    /// @brief      Finds the first occurrence of the pattern in the input range
+    ///
+    /// @param[in]  input   Input range to explore
+    ///
+    /// @tparam     Range
+    ///
+    /// @return     A subrange that meets the pattern
+    ///
     template<typename Range>
     auto operator()(Range const &input) const noexcept { return (*this)(std::begin(input), std::end(input)); }
 
