24a3cc763cc291ed62f8eb245916ea496f92c403
Denis Pronin <dannftk@yandex.ru>
2020-04-04 11:37:34 +0300

    considerably increased performance for slow stream-oriented mode (fallback variant if virtual memory is insufficient to map an input file or if stdin is used)

--- src/splitters/stream_splitter.hpp
+++ src/splitters/stream_splitter.hpp
@@ -30,21 +30,6 @@ public:
     }
 
     ///
-    /// @brief      Gets a string-like token from the splitter
-    ///
-    /// @return     The token
-    ///
-    template <typename U = ValueType>
-    typename std::enable_if<std::is_same<U, char>::value, std::string>::type
-        operator()()
-    {
-        // @info you can use std::getline here instead of using Range-based splitter
-        std::string token;
-        splitter_(std::back_inserter(token));
-        return token;
-    }
-
-    ///
     /// @brief      Gets a token in a shape of vector of items
     ///
     /// @return     The token
@@ -75,6 +60,55 @@ public:
 
 private:
     RangeSplitter<std::istream_iterator<ValueType>> splitter_;
+};
+
+///
+/// @brief      This class is a specialization of template class StreamSplitter
+///
+template<>
+class StreamSplitter<char>
+{
+public:
+    ///
+    /// @brief      Constructs stream splitter
+    ///
+    /// @param[in]  is     Input stream
+    /// @param[in]  delim  The delimiter specifying a separator between tokens
+    ///
+    explicit StreamSplitter(std::istream &is, char delim = '\n')
+        : is_(is), delim_(delim)
+    {
+    }
+
+    ///
+    /// @brief      Gets a string-like token by means of std::getline
+    ///
+    /// @return     The token
+    ///
+    auto operator()()
+    {
+        std::string token;
+        std::getline(is_, token, delim_);
+        return token;
+    }
+
+    ///
+    /// @brief      Bool conversion operator. Checks if the stream is not exhausted
+    ///
+    /// @returns    True if the stream is not finished yet, False otherwise
+    ///
+    operator bool() const noexcept { return static_cast<bool>(is_); }
+
+    ///
+    /// @brief      Bool conversion operator. Checks if the stream is exhausted
+    ///
+    /// @returns    True if the stream is exhausted, False otherwise
+    ///
+    bool operator!() const noexcept { return !is_; }
+
+private:
+    std::istream &is_;
+    char         delim_;
 };
 
 } // namespace mtfind
